870d3cf480bc583a94cbfa980ce7cd6aeffbcadf
Lucas Hom <lucasrylanhom@gmail.com>
2026-04-15 16:55:19 -0700

    delete and get by job

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -51,13 +51,26 @@ const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
+const findUserByJob = (job) => {
+  return users["users_list"].filter((user) => user["job"] === job);
+};
+
+// edited for name and job
 app.get("/users", (req, res) => {
   const name = req.query.name;
+  const job = req.query.job;
+
   if (name != undefined) {
     let result = findUserByName(name);
     result = { users_list: result };
     res.send(result);
-  } else {
+  } 
+  else if (job != undefined) {
+    let result = findUserByJob(job);
+    result = { users_list: result };
+    res.send(result);
+  } 
+  else {
     res.send(users);
   }
 });
@@ -74,3 +87,33 @@ app.get("/users/:id", (req, res) => {
     res.send(result);
   }
 });
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+
+const deleteUserById = (id) => {
+  const index = users["users_list"].findIndex(
+    (user) => user.id === id
+  );
+
+  const deletedUser = users["users_list"].splice(index, 1);
+  return deletedUser;
+};
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  const deletedUser = deleteUserById(id);
+
+  res.send({
+    deletedUser
+  });
+})
